6aec7b29222facf2e41fcae741bfe1ec47974737
anilergan <ergananil@gmail.com>
2024-06-01 16:42:34 +0300

    commit

--- tic-tac-toe-dp/main.py
+++ tic-tac-toe-dp/main.py
@@ -1,5 +1,19 @@
-from play import play_tic_tac_toe 
+# Following code is to run the game in terminal (recommended)
 
-play_object = play_tic_tac_toe()
+# from play import play_tic_tac_toe 
+# play_object = play_tic_tac_toe()
+# play_object.play()
 
-play_object.play()
+
+# Following code is to run the game in user interface (recommended)
+
+from PyQt6.QtWidgets import QApplication
+from game_gui import AppWindow
+from sys import argv
+
+app = QApplication(argv)
+
+window = AppWindow()
+window.show()
+
+app.exec()
